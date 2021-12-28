c1e1a79de3e32c54953ea532242791c80b418a2d
Abderrahmen MCHAYAA <7540557-AbdouMchayaa@users.noreply.gitlab.com>
2021-12-28 11:10:05 +0100

    send emails

--- test-docker/src/Controller/MailerController.php
+++ test-docker/src/Controller/MailerController.php
@@ -4,17 +4,26 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MailerController extends AbstractController
 {
     /**
-     * @Route("/mailer", name="mailer")
+     * @Route("/mail", name="mail")
      */
-    public function index(): Response
+    public function index(MailerInterface $mailer): Response
     {
+        $email = (new Email())
+            ->from('hello@example.com')
+            ->to('you@example.com')
+            ->subject('Test de MailDev')
+            ->text('Ceci est un mail de test');
+        $mailer->send($email);
+
         return $this->render('mailer/index.html.twig', [
-            'controller_name' => 'MailerController',
+            'controller_name' => 'MailController',
         ]);
     }
 }
